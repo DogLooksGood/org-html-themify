44534fa8263c864ee323000cfc9f7e5b7313a127
shitianshu <doglooksgood@gmail.com>
2021-01-17 00:38:17 +0800

    persistent dark/light in cookie

--- org-html-themify.js
+++ org-html-themify.js
@@ -1,17 +1,43 @@
+let cookie = document.cookie.split('; ').find(r => r.startsWith('org_html_themify_theme'))
+let theme = 'light'
+
+if (cookie) {
+  theme = cookie.split('=')[1]
+}
+
 let toggleThemeBtn = document.getElementById('toggle-theme')
 let toggleTocBtn = document.getElementById('toggle-toc')
 
-toggleThemeBtn.addEventListener('click', function() {
-  if (document.body.dataset.theme == 'dark') {
-    document.body.dataset.theme = 'light'
-    toggleThemeBtn.innerHTML = 'dark theme'
-  } else {
-    document.body.dataset.theme = 'dark'
-    toggleThemeBtn.innerHTML = 'light theme'
-  }
+if (theme == 'light') {
+  useLightTheme();
+} else {
+  useDarkTheme();
+}
 
+function transition() {
   document.body.classList.add('theme-transition')
   setTimeout(() => document.body.classList.remove('theme-transition'), 500)
+}
+
+function useLightTheme(theme) {
+  document.body.dataset.theme = 'light'
+  toggleThemeBtn.innerHTML = 'dark theme'
+  document.cookie = 'org_html_themify_theme=light'
+}
+
+function useDarkTheme(theme) {
+  document.body.dataset.theme = 'dark'
+  toggleThemeBtn.innerHTML = 'light theme'
+  document.cookie = 'org_html_themify_theme=dark'
+}
+
+toggleThemeBtn.addEventListener('click', function() {
+  transition()
+  if (document.body.dataset.theme == 'light') {
+    useDarkTheme();
+  } else {
+    useLightTheme();
+  }
 })
 
 
